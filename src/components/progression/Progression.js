c9331e450daf18e864d97b38f7dabd0e2da5adda
totorototo <el.totorototo@gmail.com>
2020-05-06 17:54:27 +0200

    chore: add radial progress bar

--- src/components/progression/Progression.js
+++ src/components/progression/Progression.js
@@ -1,16 +1,130 @@
-import React from "react";
+import React, { Fragment, useEffect, useState } from "react";
 
 import styled from "./style";
 
+const RADIUS_OFFSET = 100;
+
+const RadialProgessBar = ({ width = 200, height = 200, data }) => {
+  const [enhancedData, setEnhancedData] = useState();
+
+  useEffect(() => {
+    if (!data) return;
+    const enhancedData = data.map((item, index) => {
+      const radius = 950 / 2 - RADIUS_OFFSET * index;
+      const strokeDasharray = 2 * Math.PI * radius;
+      const strokeDashoffset =
+        strokeDasharray * (1 - (item.percent * 0.75) / 100);
+      return { ...item, strokeDasharray, strokeDashoffset, radius };
+    });
+
+    setEnhancedData(enhancedData);
+  }, [data]);
+
+  return (
+    <svg width={width} height={height} viewBox="0 0 1000 1000">
+      {enhancedData &&
+        enhancedData.map((item, index) => (
+          <Fragment key={index}>
+            <text
+              x={500 + 50}
+              y={100 * index + 40}
+              style={{
+                fontSize: 40,
+                letterSpacing: 1,
+                fontWeight: "lighter",
+                fill: "white",
+              }}
+            >
+              {item.label}
+            </text>
+            <circle
+              id={`${index}-progress`}
+              key={`${index}-inside`}
+              cx={500}
+              cy={500}
+              r={item.radius}
+              strokeWidth="1"
+              stroke="#ffffff"
+              fill="none"
+              strokeDasharray={item.strokeDasharray}
+              strokeLinecap="round"
+              strokeDashoffset={item.strokeDasharray * (1 - 75 / 100)}
+            />
+            <circle
+              key={`${index}-outside`}
+              cx={500}
+              cy={500}
+              r={item.radius}
+              strokeWidth={1000 / 20}
+              stroke="#357597"
+              fill="none"
+              strokeDasharray={item.strokeDasharray}
+              strokeDashoffset={item.strokeDashoffset}
+            />
+            <text key={`${index}-text-circle`}>
+              <textPath
+                style={{
+                  fontSize: 40,
+                  letterSpacing: 1,
+                  fontWeight: "lighter",
+                  fill: "white",
+                }}
+                href={`#${index}-progress`}
+                startOffset={`${item.percent * 0.75 + 1}%`}
+              >
+                {`${item.percent.toFixed(0)}%`}
+              </textPath>
+            </text>
+          </Fragment>
+        ))}
+    </svg>
+  );
+};
+
 const Progression = ({ className, progression, routeAnalytics }) => {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    if (!routeAnalytics || !progression) return;
+
+    const updatedData = [
+      {
+        label: "distance",
+        total: routeAnalytics.distance,
+        progression: progression[0],
+        percent: (progression[0] * 100) / routeAnalytics.distance,
+      },
+      {
+        label: " elevation gain",
+        total: routeAnalytics.elevation.positive,
+        progression: progression[1],
+        percent:
+          (progression[1] * 100) / routeAnalytics.elevation.positive <= 100
+            ? (progression[1] * 100) / routeAnalytics.elevation.positive
+            : 100,
+      },
+      {
+        label: "elevation loss",
+        total: routeAnalytics.elevation.negative,
+        progression: progression[2],
+        percent:
+          (progression[2] * 100) / routeAnalytics.elevation.negative <= 100
+            ? (progression[2] * 100) / routeAnalytics.elevation.negative
+            : 100,
+      },
+    ];
+    setData(updatedData);
+  }, [routeAnalytics, progression]);
+
   return (
     <div className={className}>
-      <div>{`${progression[0].toFixed(2)} km`}</div>
+      <RadialProgessBar data={data} />
+      {/* <div>{`${progression[0].toFixed(2)} km`}</div>
       <div>{`${progression[1].toFixed(0)} m`}</div>
       <div>{`${progression[2].toFixed(0)} m`}</div>
       <div>{`${routeAnalytics.distance.toFixed(2)} km`}</div>
       <div>{`${routeAnalytics.elevation.positive.toFixed(0)} m`}</div>
-      <div>{`${routeAnalytics.elevation.negative.toFixed(0)} m`}</div>
+      <div>{`${routeAnalytics.elevation.negative.toFixed(0)} m`}</div> */}
     </div>
   );
 };
--- src/components/progression/style.js
+++ src/components/progression/style.js
@@ -10,5 +10,7 @@ export default (Component) => styled(Component)`
   color: #ffffff94;
   flex-direction: column;
   font-family: roboto;
+  // background-color: pink;
+  position: relative;
   font-size: ${(props) => props.theme.space[3]}px;
 `;
