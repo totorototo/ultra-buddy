98dd9a5ea7544cf3905e8bfcdeaf71da69d5a0b7
Toto RoToto <el.totorototo@gmail.com>
2020-06-23 22:37:08 +0200

    fiix: atom persistance

--- src/model/domain.js
+++ src/model/domain.js
@@ -3,6 +3,7 @@ import { atom } from "recoil";
 const domain = atom({
   key: "domain",
   default: { x: { min: 0, max: 0 }, y: { min: 0, max: 0 } },
+  persistence_UNSTABLE: { type: true },
 });
 
 export default domain;
